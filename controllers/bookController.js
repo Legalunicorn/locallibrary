3ebfa4b61dfa577d2c72ceafd4142bc0dfe2e0c7
Nathan Yeo <nyeo700@gmail.com>
2024-05-16 11:08:20 +0800

    book delete challenge

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -160,19 +160,60 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
-  // const [book. allAuthors, allGenres]
+  // res.send("NOT IMPLEMENTED: Book delete GET");
+  //gets the page to confirm the book deletion
+  // in order to delete a book, all book instances must be deleted first
+  const [book, allBookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    BookInstance.find({book:req.params.id},"imprint status").exec(),
+  ])
+
+  if (book==null){
+    //no suh book
+    res.redirect("/catalog/books")
+  }
+
+  res.render("book_delete",{
+    title: "Delete Books",
+    book: book,
+    book_instances: allBookInstances
+  })
+
+
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  // res.send("NOT IMPLEMENTED: Book delete POST");
+
+  //sends a post reqest to the DB to delete the book
+  // here we must check if there are still book instances or note
+  const [book, allBookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    Book.find({author:req.params.id},"imprint status").exec(),
+  ])
+
+  if (allBookInstances.length>0){
+    //delete book instances first 
+    res.render("book_delete",{
+      title: "Delete Book",
+      book: book,
+      book_instances: allBookInstances
+    })
+    return;
+  } else{
+      // proceed with book deletion
+      await Book.findByIdAndDelete(req.body.bookid);
+      res.redirect('/catalog/books');
+  }
+
+
 });
 
 // Display book update form on GET.
 exports.book_update_get = asyncHandler(async (req, res, next) => {
   // res.send("NOT IMPLEMENTED: Book update GET");
-  const [book, allAuthors,allGenres] = await promise.all([
+  const [book, allAuthors,allGenres] = await Promise.all([
     Book.findById(req.params.id).populate("author").exec(),
     Author.find().sort({family_name:1}).exec(),
     Genre.find().sort({name:1}).exec()
@@ -190,7 +231,7 @@ exports.book_update_get = asyncHandler(async (req, res, next) => {
   })
 
   res.render("book_form",{
-    title: "Update Book".
+    title: "Update Book",
     authors: allAuthors,
     genres:allGenres,
     book:book
@@ -224,24 +265,44 @@ exports.book_update_post  = [
 
   asyncHandler(async (req,res,next)=>{
     //extract validation erros 
-    const erros  = validationResult(req);
+    const errors  = validationResult(req);
     //create book object with escaped/trimmed data and old id
 
     const book = new Book({
       title:req.body.title,
       author:req.body.author,
       summary:req.body.summary,
-      isbn.req.body.isbn,
+      isbn:req.body.isbn,
       genre: typeof req.body.genre === "undefined" ? [] : req.body.genre,
       _id: req.params.id, // THIS IS REQUIRED OR A NEW ID WILL BE ASSIGEND
-
-      if (!errors.isEmpty()){
-        //there are errors 
-        const[allAuthors, alLGenres] = await Promise.all([
-          Author.find().sort({family_name:1}).exec(),
-          Genre.find().sort({name:1}).exec()
-        ])
-      }
+      
     })
+    if (!errors.isEmpty()){
+      //there are errors 
+      const [allAuthors, alLGenres] = await Promise.all([
+        Author.find().sort({family_name:1}).exec(),
+        Genre.find().sort({name:1}).exec()
+      ]);
+
+      for (const genre of allGenres){
+        if (book.genre.indexOf(genre._id)>-1){
+          genre.checked = "true";
+        }
+      }
+
+      res.render("book_form",{
+        title:"Update book",
+        authors: allAuthors,
+        genres:allGenres,
+        book:book,
+        errors:errors.array(),
+      });
+      return;
+
+    } else{
+      const updatedBook = await Book.findByIdAndUpdate(req.params.id,book,{});
+      res.redirect(updatedBook.url)
+    }
+
   })
 ]
